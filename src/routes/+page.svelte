bb29e52dd3098d7e9a9dbe66c709453d96d513cb
Lucas <lucas@somfic.dev>
2026-03-11 22:59:14 +0100

    ✨ bakfiets
    semver: minor

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<img src="/image.png" />
+<h1>d66bakfietswijf.nl</h1>
+
+<style lang="scss">
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+        min-height: 100vh;
+        display: flex;
+        flex-grow: 1;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
